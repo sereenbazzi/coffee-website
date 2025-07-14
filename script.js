daacb108f84cefbf776bf06b739a6472b7c78888
Sereen Bazzi <sereenbazzi@gmail.com>
2025-07-14 20:05:25 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const navLinks= document.querySelectorAll(".nav-menu .nav-link")
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click" , ()=> {
+  //Toggle mobile menu visibility
+document.body.classList.toggle("show-mobile-menu");
+});
+//Close menu when the close is clicked
+menuCloseButton.addEventListener("click" , ()=> menuOpenButton.click ());
+//close menu when the nav link is clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", () => menuOpenButton.click());
+})
+// initialize swiper
+ const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween: 25,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor: true,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        } ,
+        768: {
+            slidesPerView: 2
+        } ,
+        1024 : {
+            slidesPerView: 3
+        }
+    }
+  });
